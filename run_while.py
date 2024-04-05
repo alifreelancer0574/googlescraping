395ad0085bff0f5710951c2af62836848c8c8e57
alifreelancer0574 <90083041+alifreelancer0574@users.noreply.github.com>
2024-04-06 01:32:43 +0500

    update_run_shile

--- run_while.py
+++ run_while.py
@@ -1,28 +1,31 @@
-import psutil
 import subprocess
 import time
 
-def is_process_running(process_name):
-    for proc in psutil.process_iter(['pid', 'name']):
-        if process_name.lower() in proc.info['name'].lower():
-            return True
-    return False
 
-def start_process(process_path):
-    subprocess.Popen(['python', process_path])
+def is_process_running(process_name):
+    try:
+        output = subprocess.check_output(['pgrep', '-f', process_name])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def start_process(process_name):
+    subprocess.Popen(['python3', process_name])
+
 
 def main():
     process_name = "google_scraping.py"
-    process_path = "./google_scraping.py"  # Change this to the actual path of your checup.py script
 
     while True:
         if not is_process_running(process_name):
             print("Process is not running, starting...")
-            start_process(process_path)
+            start_process(process_name)
         else:
             print("Process is running.")
 
         time.sleep(5)  # Check every 5 seconds
+
 
 if __name__ == "__main__":
     main()
